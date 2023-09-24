5d4b77b005136d2cbe9ff20843f88b852b4565cc
Sheilendra SIngh <sheilendra31@gmail.com>
2023-09-24 22:52:24 +0530

    updated js

--- Level1/Task1/index.js
+++ Level1/Task1/index.js
@@ -1,6 +1,23 @@
+let openSideBarButton = document.getElementById("navlistopn");
+let closeSideBarButton = document.getElementById("navclosbtn");
+
+openSideBarButton.addEventListener("click", () => {
+  //function calling for opening side bar
+  openNav();
+});
+
+closeSideBarButton.addEventListener("click", () => {
+  //function calling for closing side bar
+  closeNav();
+});
+
+// Created function for opening side bar
+
 function openNav() {
   document.getElementById("mySideNav").style.width = "45vw";
 }
+
+// Created function for closing side bar
 
 function closeNav() {
   document.getElementById("mySideNav").style.width = "0";
